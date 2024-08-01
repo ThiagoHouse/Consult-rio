34dbb7aa2e047f5c06b04fb1014fd8b3d7857897
ThiagoHouse <thiago.cruz@audeedu.com.br>
2024-07-31 22:46:01 -0300

    Wip

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -4,28 +4,29 @@
       img-width="1024" img-height="480" style="text-shadow: 1px 1px 2px #333;" @sliding-start="onSlideStart"
       @sliding-end="onSlideEnd">
       <!-- Text slides with image -->
-      <b-carousel-slide 
-        caption="Fisioterapia" 
-        text="Cuidando da sua saúde com excelência e dedicação">
-        <img slot="img" class="d-block img-fluid w-100" width="1024" height="480" src="../assets/bebe.jpeg" alt="image slot">
+      <b-carousel-slide caption="Fisioterapia" text="Cuidando da sua saúde com excelência e dedicação">
+        <img style="max-height: 400px;" slot="img" class="mx-auto d-block img-fluid" src="../assets/bebe.jpeg" alt="image slot" />
       </b-carousel-slide>
 
       <!-- Slides with custom text -->
-      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=57">
-        <h1>Fisioterapia</h1>
+      <b-carousel-slide>
+        <img style="max-height: 400px;" slot="img" class="mx-auto d-block img-fluid" width="1024" height="180" src="../assets/fisio.jpg"
+          alt="image slot">
       </b-carousel-slide>
 
       <!-- Slides with image only -->
-      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
+      <b-carousel-slide>
+        <img style="max-height: 400px;" slot="img" class="mx-auto d-block img-fluid" src="../assets/esquerda.jpg" alt="image slot">
+      </b-carousel-slide>
 
       <!-- Slides with img slot -->
       <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
-      <b-carousel-slide>
+      <!-- <b-carousel-slide>
         <template #img>
-          <img class="d-block img-fluid w-100" width="1024" height="480" src="https://picsum.photos/1024/480/?image=55"
+          <img class="d-block img-fluid w-100" src="https://picsum.photos/1024/980/?image=55"
             alt="image slot">
         </template>
-      </b-carousel-slide>
+</b-carousel-slide> -->
 
       <!-- Slide with blank fluid image to maintain slide aspect ratio -->
       <!-- <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
@@ -61,3 +62,7 @@ export default {
   }
 }
 </script>
+
+<style>
+
+</style>
